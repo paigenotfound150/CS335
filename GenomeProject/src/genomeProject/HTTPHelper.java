730c77bad11b72079fbdad2b1877e8e959ad68a6
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2024-02-09 12:36:15 -0500

    Moving http stuff to its own file

---
+++ GenomeProject/src/genomeProject/HTTPHelper.java
@@ -0,0 +1,39 @@
+package genomeProject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HTTPHelper {
+	
+	/* The sendGET method creates and sends an HTTP GET request to NCBI's E-Utils APi.
+	 * It accepts an API query, formatted as a string, and prints out
+	 * the response from the APi.*/
+	public static void sendGET(String query) throws IOException {
+		// Create connection and send request
+		URL obj = new URL(query);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		int responseCode = con.getResponseCode();
+		
+		System.out.println("GET Response Code :: " + responseCode);
+		// Read response
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// If successful, print result
+			System.out.println(response.toString());
+		} else {
+		System.out.println("GET request did not work");
+		}
+	}
+}
--- GenomeProject/src/genomeProject/Main.java
+++ GenomeProject/src/genomeProject/Main.java
@@ -1,50 +1,26 @@
 package genomeProject;
 
 import java.io.IOException;
-import java.net.URL;
-import java.net.HttpURLConnection;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
+import genomeProject.HTTPHelper;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
 		String header = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/";
 		String command = "efetch.fcgi?db=nuccore&id=KR132597.1&rettype=fasta&retmode=json";
 		String query = header.concat(command);
-		sendGET(query);
 		
-	}
-	
-	/* The sendGET method creates and sends an HTTP GET request to NCBI's E-Utils APi.
-	 * It accepts an API query, formatted as a string, and prints out
-	 * the response from the APi.*/
-	private static void sendGET(String query) throws IOException {
-		// Create connection and send request
-		URL obj = new URL(query);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-		con.setRequestMethod("GET");
-		int responseCode = con.getResponseCode();
-		
-		System.out.println("GET Response Code :: " + responseCode);
-		// Read response
-		if (responseCode == HttpURLConnection.HTTP_OK) { // success
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-
-			// If successful, print result
-			System.out.println(response.toString());
-		} else {
-		System.out.println("GET request did not work");
+		HTTPHelper HTTPRequestHandler = new HTTPHelper();
+		try {
+			HTTPRequestHandler.sendGET(query);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
+	
 	
 }
 
