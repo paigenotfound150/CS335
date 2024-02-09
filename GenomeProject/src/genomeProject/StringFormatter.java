35c740a0837a2180e7ebf7a5702ea53a3de2a3a4
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2024-02-09 12:49:41 -0500

    changing comment

--- GenomeProject/src/genomeProject/StringFormatter.java
+++ GenomeProject/src/genomeProject/StringFormatter.java
@@ -2,7 +2,7 @@ package genomeProject;
 
 class StringFormatter {
 	
-	/* removeWhiteSpace removes any whitespace existing in a string. */
+	/* removeWhiteSpace removes any whitespace in a string. */
 	public static String removeWhiteSpace(String original) {
 		String str = original.replaceAll("\\s", "");
 		return str;
