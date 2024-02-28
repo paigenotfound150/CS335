76f069f48a04b164eb7f13a443979b9f1bd19249
linalaghzaoui <laghzaou@simmons.edu>
2024-02-28 12:59:19 -0500

    query result
    3rd sprint

--- GenomeProject/src/genomeProject/Main.java
+++ GenomeProject/src/genomeProject/Main.java
@@ -1,56 +1,60 @@
 package genomeProject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         String header = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/";
-        
-        // eSearch builder
-        String dbQuery = entrezQuery.main();
-        String esearchCommand = "esearch.fcgi?db=nuccore&term=".concat(dbQuery);
-        String esearchQuery = header.concat(esearchCommand);
-        String esearchResponse = "";
-        
-        // eSearch send
-        try {
-            esearchResponse = HTTPHelper.sendGET(esearchQuery);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        // eFetch builder
-        String uidList = StringFormatter.uidFinder(esearchResponse);
-        String efetchCommand = "efetch.fcgi?db=nuccore&id=" + uidList + "&rettype=fasta";
-        String efetchQuery = header.concat(efetchCommand);
-        String formatted_query = StringFormatter.removeWhiteSpace(efetchQuery);
-        String response = "";
-        
-        // eFetch send
-        try {
-            response = HTTPHelper.sendGET(formatted_query);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        // To string method prints it with specified indentation
-        // (not sure what this does but I trust paige <3 -Anna)
-        String s = response.replaceAll("(?<=[{:,])|(?=[:,}])", "\"");
-        System.out.println(s);
+        List<QueryResult> queryResults = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in); // Use a single Scanner instance
 
-        // After the NCBI fetch, now ask for primers
-        PrimerInputHandler primerInputHandler = new PrimerInputHandler();
+        // Adjusted entrezQuery to use the existing scanner instance
+        String dbQuery = entrezQuery.main(scanner);
+        String esearchCommand = "esearch.fcgi?db=nuccore&term=" + dbQuery + "&retmode=json&rettype=uilist";
+        String esearchQuery = header + esearchCommand;
+
+        try {
+            String esearchResponse = HTTPHelper.sendGET(esearchQuery);
+            String uidList = StringFormatter.uidFinder(esearchResponse);
+
+            String efetchCommand = "efetch.fcgi?db=nuccore&id=" + uidList + "&rettype=fasta&retmode=text";
+            String efetchQuery = header + efetchCommand;
+            String fastaResponse = HTTPHelper.sendGET(efetchQuery);
+
+            queryResults = parseFASTAResponse(fastaResponse);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Instantiate PrimerInputHandler using the existing scanner instance
+        PrimerInputHandler primerInputHandler = new PrimerInputHandler(scanner);
         primerInputHandler.getPrimersFromUser();
-        String forwardPrimer = primerInputHandler.getForwardPrimer();
-        String reversePrimer = primerInputHandler.getReversePrimer();
+
+        // Print descriptions and sequences with labels after receiving primer inputs
+        for (QueryResult result : queryResults) {
+            System.out.println("Description: " + result.getDescription());
+            System.out.println("Sequence: " + result.getSequence() + "\n");
+        }
+
+        scanner.close(); // Close the scanner here, at the end of all input operations
+    }
+
+    private static List<QueryResult> parseFASTAResponse(String fastaResponse) {
+        List<QueryResult> results = new ArrayList<>();
+        String[] records = fastaResponse.trim().split(">");
         
-        System.out.println(forwardPrimer + reversePrimer);
-
-        // Use the primers for further processing as needed
-        // ...
-
-        // Since we are done with all input operations, close the Scanner
-        primerInputHandler.closeScanner();
+        for (String record : records) {
+            if (!record.isEmpty()) {
+                int firstLineEnd = record.indexOf("\n");
+                String description = record.substring(0, firstLineEnd).trim();
+                String sequence = record.substring(firstLineEnd).replaceAll("\\s+", "").trim();
+                results.add(new QueryResult(description, sequence));
+            }
+        }
+        return results;
     }
 }
--- GenomeProject/src/genomeProject/PrimerInputHandler.java
+++ GenomeProject/src/genomeProject/PrimerInputHandler.java
@@ -7,12 +7,12 @@ public class PrimerInputHandler {
     private String reversePrimer;
     private Scanner sc;
 
-    public PrimerInputHandler() {
-        sc = new Scanner(System.in); // Initialize the Scanner here
+    // Modify constructor to accept Scanner object
+    public PrimerInputHandler(Scanner sc) {
+        this.sc = sc; // Use the passed Scanner object
     }
 
     public void getPrimersFromUser() {
-        // Prompt user for forward primer
         System.out.println("Please enter your forward primer (5' to 3' on + strand)");
         forwardPrimer = sc.nextLine();
         while (!isValidPrimer(forwardPrimer)) {
@@ -20,19 +20,15 @@ public class PrimerInputHandler {
             forwardPrimer = sc.nextLine();
         }
 
-        // Prompt user for reverse primer
         System.out.println("Please enter your reverse primer (5' to 3' on - strand):");
         reversePrimer = sc.nextLine();
         while (!isValidPrimer(reversePrimer)) {
             System.out.println("Invalid input. Primer must only contain A, T, C, or G. Please try again.");
             reversePrimer = sc.nextLine();
         }
-        
-        // Do not close the scanner here
     }
 
     private boolean isValidPrimer(String primer) {
-        // Check if the primer sequence contains only A, T, C, or G
         return primer.matches("[ATCG]+");
     }
 
@@ -43,9 +39,6 @@ public class PrimerInputHandler {
     public String getReversePrimer() {
         return reversePrimer;
     }
-    
-    // Call this method to close the scanner when you're done with all input operations
-    public void closeScanner() {
-        sc.close();
-    }
+
+    // Removed the closeScanner method as it's no longer necessary here
 }
